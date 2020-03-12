3aa59940a676afb0d86cafe6c9731199197a5315
Basemera <phiona.basemera@andela.com>
2020-03-13 00:05:18 +0300

    Add heroku db

--- propertyManagement/settings.py
+++ propertyManagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,3 +149,5 @@ STATIC_URL = '/static/'
 GRAPHENE = {
     'SCHEMA': 'propertyManagement.schema.schema',
 }
+
+django_heroku.settings(locals())
