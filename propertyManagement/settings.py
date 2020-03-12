759f25dda58b6033eea9b44d6c9614fa269c90f5
Basemera <phiona.basemera@andela.com>
2020-03-13 02:03:14 +0300

    Add static file

--- propertyManagement/settings.py
+++ propertyManagement/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'propertyManagement.urls'
@@ -181,6 +183,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 GRAPHENE = {
     'SCHEMA': 'propertyManagement.schema.schema',
