869bf673b41f3d3acdcf2fa2fa7f2ac6c9db219b
Basemera <phiona.basemera@andela.com>
2020-03-13 01:58:06 +0300

    Add static file

--- propertyManagement/settings.py
+++ propertyManagement/settings.py
@@ -170,7 +170,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 GRAPHENE = {
     'SCHEMA': 'propertyManagement.schema.schema',
