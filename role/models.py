b3f9dd3147a160b3cb4b87476f4cb1ba9ca0a3fe
Basemera <phiona.basemera@andela.com>
2020-03-13 12:43:27 +0300

    Fix role model

--- role/models.py
+++ role/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Role(models.Model):
-    role_name = models.TextField(max_length = 40)
+    role_name = models.TextField(max_length = 40, default='Guest')
     description = models.TextField()
