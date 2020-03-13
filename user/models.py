7ca980579851423fd1874c1b364120c378fcc583
Basemera <phiona.basemera@andela.com>
2020-03-13 12:48:27 +0300

    Fix role model

--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-role = models.ForeignKey('role.Role', on_delete=models.CASCADE)
+role = models.ForeignKey('role.Role', on_delete=models.CASCADE, default=1)
 role.contribute_to_class(User, 'role')
