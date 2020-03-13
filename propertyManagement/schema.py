5f3c51de3fdcadfd00dd937fd281c7206db15812
Basemera <phiona.basemera@andela.com>
2020-03-13 15:12:48 +0300

    Add role

--- propertyManagement/schema.py
+++ propertyManagement/schema.py
@@ -1,9 +1,10 @@
 import graphene
 import user.schema
-import role.schema
+# import role.schema
+from role.schema.mutations.role import Mutation as RoleMutation
 
 # Mutation for sending the data to the server.
-class Mutation(user.schema.Mutation, role.schema.mutation.Mutation, graphene.ObjectType):
+class Mutation(user.schema.Mutation, RoleMutation, graphene.ObjectType):
     pass
 
 # Create schema
--- role/schema/mutations/role.py
+++ role/schema/mutations/role.py
@@ -18,6 +18,9 @@ class CreateRole(graphene.Mutation):
         description =graphene.String()
 
     def mutate(self, info, role_name, description):
+        roleInstance = Role.objects.get(role_name=role_name)
+        if roleInstance:
+            return CreateRole(role=roleInstance)
         role = Role(role_name=role_name, description=description)
         role.save()
         return CreateRole(role=role)
