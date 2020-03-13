03eb629948a5672a26d3d3e5c3d2d88ad8369321
Basemera <phiona.basemera@andela.com>
2020-03-13 12:39:05 +0300

    Fix wsgi

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn propertyManagement.wsgi —-log-file -
-release: python manage.py makemigrations --noinput && python manage.py migrate --noinput
+release: python manage.py makemigrations && python manage.py migrate
--- propertyManagement/settings.py
+++ propertyManagement/settings.py
@@ -15,6 +15,8 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -172,7 +174,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
