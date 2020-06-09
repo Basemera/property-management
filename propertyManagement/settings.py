522f0ab82e24deac29b89a16dd62a76096d7c28a
Basemera <bsmrrachel@gmail.com>
2020-06-09 23:41:51 +0300

    Update settings

--- propertyManagement/settings.py
+++ propertyManagement/settings.py
@@ -101,21 +101,44 @@ WSGI_APPLICATION = 'propertyManagement.wsgi.application'
 # }
 
  
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'new_user@properties;',
+        # 'NAME' : 'properties',
+        # 'alias' : 'properties',
+        # 'USER': 'new_user',
+        # 'PASSWORD': 'root',
+        # 'HOST' : 'localhost',
+        # 'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
+        # 'PORT': '8889',
+        # 'OPTIONS': {
+        # 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
+
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-# 'NAME': 'new_user@properties;',
-'NAME' : 'properties',
-# 'alias' : 'properties',
-'USER': 'new_user',
-'PASSWORD': 'root',
-'HOST' : 'localhost',
-# 'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
-'PORT': '8889',
-'OPTIONS': {
-'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-}
-}
+    'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
+        'ENGINE' : os.environ.get('ENGINE', 'django.db.backends.postgresql'),
+        # 'NAME': 'new_user@properties;',
+        # 'NAME' : 'properties',
+        'NAME' : os.environ.get('NAME', 'enju-properties'),
+        # 'alias' : 'properties',
+        # 'USER': 'new_user',
+        'USER' : os.environ.get('USER', 'postgres'),
+        # 'PASSWORD': 'root',
+        'PASSWORD' : os.environ.get('PASSWORD', 'NtZiXm0LqgXCx8YYzEkI'),
+        # 'HOST' : 'localhost',
+        'HOST' : os.environ.get('HOST', 'enju-properties.c5l67bhqsieq.us-east-2.rds.amazonaws.com'),
+        # 'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
+        # 'PORT': '8889',
+        'PORT' : os.environ.get('PORT', '5432'),
+        'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
 }
 
 
